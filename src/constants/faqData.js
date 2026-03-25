d5c1a28da74132888f0afa39b7290b5d27dce7e3
Bahar Işıklı <baharisikli@outlook.com>
2026-03-25 22:26:00 +0300

    Style(Faq): FAQ section questions added

--- src/constants/faqData.js
+++ src/constants/faqData.js
@@ -2,7 +2,7 @@ export const faqs = [
   {
     question: "GDG on Campus Marmara nedir?",
     answer:
-      "Google Developer Groups (GDG) on Campus, Google teknolojileriyle ilgilenen öğrencilerin bir araya gelerek öğrenmesini, proje geliştirmesini ve ağ kurmasını sağlayan global bir topluluk programıdır.",
+      "Google Developer Groups (GDG) on Campus, Google teknolojileriyle ilgilenen öğrencilerin bir araya gelerek öğrenmesini, proje geliştirmesini ve ağ kurmasını sağlayan global bir topluluk programıdır.Marmara Üniversitesinde GDGoc Marmara olarak bulunmaktadır.",
     color: "text-blue-500",
   },
   {
@@ -14,13 +14,43 @@ export const faqs = [
   {
     question: "Yazılım bilmeyenler de etkinliklere katılabilir mi?",
     answer:
-      "Kesinlikle! Başlangıç seviyesinden ileri seviyeye kadar her düzeyde atölye ve sunum düzenliyoruz. Sıfırdan başlamak için harika bir yerdesiniz.",
+      "Kesinlikle! Başlangıç seviyesinden ileri  seviyeye kadar her düzeyde atölye, sunum ve etkinlik düzenliyoruz. Sıfırdan başlamak için harika bir yerdesiniz.",
     color: "text-green-500",
   },
   {
-    question: "Hangi alanlarda eğitimler veriyorsunuz?",
+    question: "Hangi alanlarda eğitimler ve etkinlikler veriyorsunuz?",
     answer:
       "Web, Mobil (Flutter/Android), Yapay Zeka (AI/ML), Cloud ve Google Cloud Platform gibi birçok farklı alanda teknik ve sosyal etkinlikler düzenliyoruz.",
     color: "text-yellow-500",
+  },
+  {
+    question: "Ne tür etkinlikler düzenliyorsunuz?",
+    answer:
+      "Workshoplar, teknik eğitimler , hackathonlar ve networking etkinlikleri düzenliyoruz. Özellikle sektör içerisinden farklı isimlerle bir araya geldiğimiz network etkinliklerimiz çoğunlukta.",
+    color: "text-blue-500",
+  },
+  {
+    question: "Etkinliklerden nasıl haberdar olabilirim?",
+    answer:
+      "Sosyal medya hesaplarımızı ve web sitemizdeki etkinlik takvimini takip edebilirsiniz.",
+    color: "text-red-500",
+  },
+  {
+    question: "Etkinliklerin ve eğitimlerin sonunda sertifika veriliyor mu?",
+    answer:
+      "Belirli workshop, eğitim serileri ve etkinlikleri tamamlayanlara katılım belgesi veya dijital rozetler verilebilmektedir",
+    color: "text-green-500",
+  },
+  {
+    question: "Core Team (Çekirdek Ekip) üyesi nasıl olunur?",
+    answer:
+      "Belli dönemlerde açtığımız başvuru formlarını doldurarak seçim sürecine dahil olabilirsiniz.",
+    color: "text-yellow-500",
+  },
+  {
+    question: "Şirketler veya diğer kulüplerle iş birliği yapıyor musunuz?",
+    answer:
+      "Evet, sponsorluk ve ortak etkinlik talepleri için iletişim formunu kullanabilirsiniz.",
+    color: "text-blue-500",
   },
 ];
